0530f1d7bffdce73d2cf3cbddba1337c39e14392
Stöhr <stmar7@gifo.net>
2016-12-30 16:39:36 +0100

    added notation and data.sql-generation

--- repository/fuseki-to-i2b2/src/main/java/de/dzl/cometar/Translator.java
+++ repository/fuseki-to-i2b2/src/main/java/de/dzl/cometar/Translator.java
@@ -34,9 +34,12 @@ public class Translator {
 			query_top_concepts = readFile(query_top_concepts_input);
 			query_child_concepts = readFile(query_child_concepts_input);
 			
-			Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("target/meta.sql"), "utf-8"));
-			writer.write("DELETE FROM i2b2metadata.table_access WHERE c_table_cd LIKE 'test%';\n");
-			writer.write("DELETE FROM i2b2metadata.i2b2 WHERE sourcesystem_cd='test';\n");
+			Writer writer_meta = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("target/meta.sql"), "utf-8"));
+			writer_meta.write("DELETE FROM i2b2metadata.table_access WHERE c_table_cd LIKE 'test%';\n");
+			writer_meta.write("DELETE FROM i2b2metadata.i2b2 WHERE sourcesystem_cd='test';\n");
+			
+			Writer writer_data = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("target/data.sql"), "utf-8"));
+			writer_data.write("DELETE FROM i2b2demodata.concept_dimension WHERE sourcesystem_cd='test';\n");
 			
 			Query query = QueryFactory.create(query_top_concepts);
 			QueryEngineHTTP httpQuery = new QueryEngineHTTP(sparqlEndpoint, query);
@@ -45,10 +48,11 @@ public class Translator {
 			ArrayList<String> ancestors_names = new ArrayList<String>();
 			while (results.hasNext()) {
 				QuerySolution solution = results.next();
-				writeInsertStatement(writer, solution, ancestors_names);
+				writeInsertStatement(writer_meta, writer_data, solution, ancestors_names);
 		    }
 			
-			writer.close();
+			writer_meta.close();
+			writer_data.close();
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,63 +67,75 @@ public class Translator {
 		System.out.println("done");
 	}
     
-    public void writeChildren(Writer writer, String concept, ArrayList<String> ancestors_names) throws IOException
+    public void writeChildren(Writer writer_meta, Writer writer_data, String concept, ArrayList<String> ancestors_names) throws IOException
     {
     	String queryString = query_child_concepts.replace("<PARENT>", "<"+concept+">");
 		Query query = QueryFactory.create(queryString);
 		QueryEngineHTTP httpQuery = new QueryEngineHTTP(sparqlEndpoint, query);
 		ResultSet results = httpQuery.execSelect();
 
-		ancestors_names.add(getName(concept));
+		ancestors_names.add(concept);
 		while (results.hasNext()) {
 			QuerySolution solution = results.next();
-			writeInsertStatement(writer, solution, ancestors_names);
+			writeInsertStatement(writer_meta, writer_data, solution, ancestors_names);
 	    }    	
     }
 	
 	@SuppressWarnings("unchecked")
-	public void writeInsertStatement(Writer writer, QuerySolution solution, ArrayList<String> ancestors_names) throws IOException
+	public void writeInsertStatement(Writer writer_meta, Writer writer_data, QuerySolution solution, ArrayList<String> ancestors_names) throws IOException
 	{
 		String concept = solution.get("concept").toString();
 		String label = solution.get("label").toString().split("@")[0];
+		String notation = solution.contains("notation")?solution.get("notation").toString():"NULL";
 		String current_timestamp = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.0")).format(new Date());
 		String concept_long = "\\i2b2\\";
 		String concept_short = "\\";
 		int c_hlevel = ancestors_names.size()+1;
 		for (String ancestor_name : ancestors_names)
 		{
-			concept_long += "bb0ead2c:"+ancestor_name+"\\";
-			concept_short += ancestor_name+"\\";
+			concept_long += getName(ancestor_name, true)+"\\";
+			concept_short += getName(ancestor_name, false)+"\\";
 		}
-		concept_long += "bb0ead2c:"+getName(concept)+"\\";
-		concept_short += getName(concept)+"\\";
+		concept_long += getName(concept, true)+"\\";
+		concept_short += getName(concept, false)+"\\";
 
-		writer.write("INSERT INTO i2b2metadata.i2b2("+
-			"c_hlevel,c_fullname,c_name,c_synonym_cd,c_visualattributes,"+
-			"c_basecode,c_metadataxml,c_facttablecolumn,c_tablename,c_columnname,"+
-			"c_columndatatype,c_operator,c_dimcode,c_tooltip,m_applied_path,"+
-			"update_date,download_date,import_date,sourcesystem_cd)"+
+		writer_meta.write("INSERT INTO i2b2metadata.i2b2("+
+				"c_hlevel,c_fullname,c_name,c_synonym_cd,c_visualattributes,"+
+				"c_basecode,c_metadataxml,c_facttablecolumn,c_tablename,c_columnname,"+
+				"c_columndatatype,c_operator,c_dimcode,c_tooltip,m_applied_path,"+
+				"update_date,download_date,import_date,sourcesystem_cd)"+
 			"VALUES("+
 				c_hlevel+",'"+concept_long+"','"+label+"','N','FA',"+
-				"NULL,NULL,'concept_cd','concept_dimension','concept_path',"+
+				"'"+notation+"',NULL,'concept_cd','concept_dimension','concept_path',"+
 				"'T','LIKE','"+concept_long+"','"+concept_short+"','@',"+
 				"current_timestamp,'"+current_timestamp+"',current_timestamp,'test'"+
 			");\n");
 		
 		if (ancestors_names.size() == 0)
 		{
-			writer.write("INSERT INTO i2b2metadata.table_access("+
-				"c_table_cd,c_table_name,c_protected_access,c_hlevel,c_fullname,"+
-				"c_name,c_synonym_cd,c_visualattributes,c_facttablecolumn,c_dimtablename,"+
-				"c_columnname,c_columndatatype,c_operator,c_dimcode,c_tooltip"+
+			writer_meta.write("INSERT INTO i2b2metadata.table_access("+
+					"c_table_cd,c_table_name,c_protected_access,c_hlevel,c_fullname,"+
+					"c_name,c_synonym_cd,c_visualattributes,c_facttablecolumn,c_dimtablename,"+
+					"c_columnname,c_columndatatype,c_operator,c_dimcode,c_tooltip"+
 				")VALUES("+
-				"'test_8883d7b2','i2b2','N',1,'"+concept_long+"',"+
-				"'"+label+"','N','FA','concept_cd','concept_dimension',"+
-				"'concept_path','T','LIKE','"+concept_long+"','"+concept_short+"'"+
+					"'test_8883d7b2','i2b2','N',1,'"+concept_long+"',"+
+					"'"+label+"','N','FA','concept_cd','concept_dimension',"+
+					"'concept_path','T','LIKE','"+concept_long+"','"+concept_short+"'"+
 				");");
 		}
+		
+		if (!notation.equals("NULL"))
+		{
+			writer_data.write("INSERT INTO i2b2demodata.concept_dimension("+
+					"concept_path,concept_cd,name_char,update_date,"+
+					"download_date,import_date,sourcesystem_cd"+
+				")VALUES("+
+					"'"+concept_long+"','"+notation+"',current_timestamp,'"+current_timestamp+
+					"',current_timestamp,'test'"+
+				");\n");
+		}
 				
-		writeChildren(writer, concept, (ArrayList<String>) ancestors_names.clone());
+		writeChildren(writer_meta, writer_data, concept, (ArrayList<String>) ancestors_names.clone());
 	}
 	  
 	public static void main(String[] args) {
@@ -127,10 +143,20 @@ public class Translator {
 		t.translate();
 	}
 	
-	private String getName(String uri)
+	private String getName(String uri, boolean withPrefix)
 	{
-		String[] split = uri.split("#");
-		return split[split.length-1];
+//		String[] split = uri.split("/");
+//		return split[split.length-1];
+		String name = uri;
+		name = name.replace("http://data.dzl.de/ont/dwh#", "dzl:");
+		name = name.replace("http://purl.bioontology.org/ontology/SNOMEDCT/", "S:");
+		name = name.replace("http://loinc.org/owl#", "L:");
+		if (!withPrefix)
+		{
+			String[] split = name.split(":");
+			name = split[split.length-1];
+		}
+		return name;
 	}
 
 	private String readFile(InputStream is) throws IOException {
