19a63d4e7c0466da67c775931b071355e2a1d6bb
Stöhr <stmar7@gifo.net>
2018-06-15 11:52:36 +0200

    moved the fuseki executable out of the repository

---
+++ repository/script/startFusekiInstances.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+source $(dirname $0)/../config/conf.cfg
+
+screen -dm /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar $FUSEKIFILE --config=$CONFDIR/fuseki_cometar_config.ttl
+
+rm -rf $TEMPDIR/git
+mkdir -p $TEMPDIR/git
+env -i -- git clone -q $TTLDIRECTORY $TEMPDIR/git
+cd $TEMPDIR/git
+env -i -- git checkout master
+
+$SCRIPTDIR/add_files_to_dataset.sh -s
+
+rm -r $TEMPDIR
