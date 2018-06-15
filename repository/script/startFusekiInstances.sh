5bed54abf9b73975760d3f2458d4c66fe3d41fa3
root <root@dzl.fb11.uni-giessen.de>
2018-06-15 13:21:32 +0200

    made start script work

--- repository/script/startFusekiInstances.sh
+++ repository/script/startFusekiInstances.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-source $(dirname $0)/../config/conf.cfg
+source $(dirname $(realpath $0))/../config/conf.cfg
 
-screen -dm /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar $FUSEKIFILE --config=$CONFDIR/fuseki_cometar_config.ttl
+screen -dm /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar $FUSEKIFILE --config=$(realpath $CONFDIR/fuseki_cometar_config.ttl)
+
+sleep 5
 
 rm -rf $TEMPDIR/git
 mkdir -p $TEMPDIR/git
 env -i -- git clone -q $TTLDIRECTORY $TEMPDIR/git
 cd $TEMPDIR/git
-env -i -- git checkout master
 
-$SCRIPTDIR/add_files_to_dataset.sh -s
+for filename in ./*.ttl; do
+		echo "Adding file $(basename "$filename")."
+		curl -X POST -H "Content-Type: text/turtle;charset=utf-8" -T "$filename" -G $FUSEKILIVEDATASET/data -d default
+	done
+echo "Adding insertrules.ttl"
+curl -X POST -s -T "$CONFDIR/insertrules.ttl" -G "$FUSEKILIVEDATASET/update"
 
 rm -r $TEMPDIR
