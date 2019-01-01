c5a0ef744b1684596d08fa09543487b7d9a6a7e9
mytmark <mark.stoehr@innere.med.uni-giessen.de>
2019-01-01 16:17:15 +0100

    scrolling headings added, style tweaks

--- angular-test/CoMetaR/src/app/services/tree.service.ts
+++ angular-test/CoMetaR/src/app/services/tree.service.ts
@@ -149,38 +149,75 @@ export class TreeService {
   //appearance
   private maxLeft=0;
   private updateWidthSubject = new BehaviorSubject<number>(this.maxLeft);
-  public updateInformationDivMaxLeft(el):Observable<number>{
+  private treeDomElement:HTMLElement;
+  public registerTreeDomElement(el:HTMLElement){
+    Array.from(el.children).forEach((child:HTMLElement) => {
+      if (child.getAttribute('id') == "tree") {
+        this.treeDomElement = child;
+      }
+    });
+  }
+  public updateInformationDivMaxLeft(el:HTMLElement):Observable<number>{
     let left = this.getPosition(el).x;
     if (this.maxLeft < left) this.updateWidthSubject.next(left);
     this.maxLeft = Math.max(this.maxLeft,left);
     return this.updateWidthSubject.pipe(map(newMaxLeft => newMaxLeft - left));
   }
-  public getInformationDivWidth():number{
-    let treeWidth = document.getElementsByTagName("APP-TREE")[0].clientWidth;
-    return treeWidth-this.maxLeft-20;
-  }
-  private getPosition(el) {
+  private getPosition(el:HTMLElement) {
     let xPos = 0;
     let yPos = 0;
-    while (el.tagName != "APP-TREE") {
-      /*if (el.tagName == "body") {
-        // deal with browser quirks with body/window/document and page scroll
-        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-        var yScroll = el.scrollTop || document.documentElement.scrollTop;
-   
-        xPos += (el.offsetLeft - xScroll + el.clientLeft);
-        yPos += (el.offsetTop - yScroll + el.clientTop);
-      } else {*/
-        // for all other non-BODY elements
-        xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-        yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-      //}
-   
-      el = el.offsetParent;
+    while (el != this.treeDomElement) {
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+      el = <HTMLElement> el.offsetParent;
     }
     return {
       x: xPos,
       y: yPos
     };
+  }
+
+  public scrollHeadingsSubject$ = new Subject<string[]>();
+  public onTreeScroll(scrollTop:number){
+    let scrollHeadings:string[]=[];   
+    let visibleListItem:HTMLElement = this.treeDomElement;
+    let foundChild = true;
+
+    while (foundChild){
+      foundChild=false;
+      let childlist = this.getList(visibleListItem);
+      if (childlist) Array.from(this.getListItems(childlist)).forEach((childlistitem:HTMLElement) => {
+        /*if (this.getTitle(listitem) == "Common Dataset") {
+          console.log(this.getTitle(childlistitem));
+          console.log(childlistitem.offsetHeight+this.getPosition(childlistitem).y > scrollTop);
+          console.log(this.getPosition(childlistitem).y);
+        }*/
+        if (foundChild)return;
+        let adjustedScrollTop = scrollTop+scrollHeadings.length*25;
+        if (this.getPosition(childlistitem).y < adjustedScrollTop && childlistitem.offsetHeight+this.getPosition(childlistitem).y-25 > adjustedScrollTop){
+          visibleListItem = childlistitem;
+          scrollHeadings.push(this.getTitle(visibleListItem));
+          foundChild=true;
+        }
+      });
+    }
+    this.scrollHeadingsSubject$.next(scrollHeadings);
+  }
+  private getTopChild(listitem:HTMLElement,scrollTop):HTMLElement{
+    let topchild:HTMLElement;
+    return topchild;
+  }
+  private getList(listitem:HTMLElement):HTMLElement{
+    let list:HTMLElement;
+    Array.from(listitem.children).forEach((child:HTMLElement) => {
+      if (child.tagName == "APP-TREE-ITEM-LIST") list = child;
+    });
+    return list;
+  }
+  private getListItems(list:HTMLElement):HTMLCollection{
+    return list.children;
+  }
+  private getTitle(listitem:HTMLElement):string{
+    return listitem.getElementsByClassName("treeItemHeading")[0].getElementsByClassName("treeItemTitle")[0].textContent;
   }
 }
