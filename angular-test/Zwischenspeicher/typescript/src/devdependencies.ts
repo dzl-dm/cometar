466cf29fdbd3c6fb2c627099a3e02c4453aee39b
Stöhr <stmar7@gifo.net>
2019-04-26 14:39:59 +0200

    removed old test-files

--- angular-test/Zwischenspeicher/typescript/src/devdependencies.ts
+++
@@ -1,7 +0,0 @@
-var dev$;
-/* develblock:start */
-var jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-var dev$ = require('jquery')((new JSDOM('')).window);
-/* develblock:end */
-export default dev$;
