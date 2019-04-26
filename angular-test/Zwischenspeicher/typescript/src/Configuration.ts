4c0d2e0bd1a8fc3e0488d9babf9eb10f20d3d53a
Stöhr <stmar7@gifo.net>
2019-04-26 14:39:59 +0200

    removed old test-files

--- angular-test/Zwischenspeicher/typescript/src/Configuration.ts
+++
@@ -1,9 +0,0 @@
-class Configuration {
-    static urlShorts = {
-        "http://data.dzl.de/ont/dwh#": "dzl/",
-        "http://purl.bioontology.org/ontology/SNOMEDCT/": "snomed/",
-        "http://loinc.org/owl#": "loinc/"
-    }
-}
-
-export default Configuration;
--- angular-test/Zwischenspeicher/typescript/src/test.ts
+++
@@ -1,2 +0,0 @@
-import getTopElements from "./queries/getTopElements";
-getTopElements((a)=>console.log(a));
