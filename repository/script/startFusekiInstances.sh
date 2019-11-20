0f0275d69b78a52f593496356211a7bc87f3627b
Stöhr <stmar7@gifo.net>
2019-11-20 12:13:03 +0100

    tweaked the script responsible for adding files to fuseki servers

--- repository/script/startFusekiInstances.sh
+++ repository/script/startFusekiInstances.sh
@@ -23,11 +23,7 @@ mkdir -p "$TEMPDIR/git"
 env -i -- git clone -q "$TTLDIRECTORY" "$TEMPDIR/git"
 cd "$TEMPDIR/git"
 
-for filename in ./*.ttl; do
-		echo "Adding file $(basename "$filename")."
-		curl -X POST -H "Content-Type: text/turtle;charset=utf-8" -T "$filename" -G "$FUSEKILIVEDATASET/data" -d default
-	done
-echo "Adding insertrules.ttl"
-curl -X POST -s -T "$CONFDIR/insertrules.ttl" -G "$FUSEKILIVEDATASET/update"
+"$SCRIPTDIR/add_files_to_dataset.sh" -s -c -p "$conffile" -e "$LOGFILE"
+"$SCRIPTDIR/add_files_to_dataset.sh" -s -c -p -h "$conffile" -e "$LOGFILE"
 
 rm -rf "$TEMPDIR/git"
