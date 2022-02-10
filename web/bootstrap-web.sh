4e3e7f8f95de9eb9012c9c4f84db6a0728bd4c63
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2022-02-10 17:12:40 +0100

    Refactor app, separate web-app and proxy functions. Build docker images while in branch for testing, tag later

---
+++ web/bootstrap-web.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+## Configuration for CoMetaR application
+echo "---- BEGIN WEB CONFIGURATION ----"
+
+envsubst "\$BROWSER_FQDN" < /config/cometar-web.tmpl > /etc/nginx/conf.d/cometar.conf
+
+## Apply application environemnt configuration
+## Generate $BROWSER_FUSEKI_SERVER from components...
+# BASE_URL=$(echo -e "${BROWSER_SCHEME}://${BROWSER_FQDN}")
+# if [[ ! "${BROWSER_PORT}" ]] ; then
+#   BASE_URL=$(echo -e "${BASE_URL}:${BROWSER_PORT}")
+# fi
+# export BASE_URL="${BASE_URL}"
+# export BROWSER_FUSEKI_SERVER=$(echo -e "${BASE_URL}${BROWSER_FUSEKI_PATH}")
+envsubst "\$HREF_BRAND \$BASE_PREFIX \$ENDPOINT_BASE" < /config/cometar_config_template.json > /usr/share/nginx/html/cometar_browser/assets/config.json
+
+chown -R nginx:nginx /usr/share/nginx/html/cometar_browser
+echo "---- END WEB CONFIGURATION ----"
