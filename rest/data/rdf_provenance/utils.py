9eceb84c27484c0a01c98e90cc764660930ef01c
Mark Stöhr <mark.stoehr@innere.med.uni-giessen.de>
2022-08-19 17:50:29 +0200

    removed unused functions

--- rest/data/rdf_provenance/utils.py
+++ rest/data/rdf_provenance/utils.py
@@ -10,29 +10,6 @@ from ..mylog import mylog
 from ..queries import git_commits
 
 import os
-import subprocess
-
-def get_last_provenance_date():
-    prov_files_dir="/var/lib/cometar/provenance/output"
-    filenames=os.listdir(prov_files_dir)
-    if len(filenames) == 0:
-        ## TOOD: Define this as a variable?
-        return "2017-01-01 00:00:00"
-    filenames.sort
-    last_filename=filenames[-1]
-    last_date=last_filename[22:41]
-    return last_date
-
-# def update_provenance_data():
-#     """Call the shell scripts which search the git history and generate a file for the provenance module to read"""
-#     ## Get most recent date already captured and write the updates to a provenance data file
-#     mylog("os.environ[COMETAR_PROD_DIR]: {}".format(os.environ["COMETAR_PROD_DIR"]))
-#     p = subprocess.Popen(["bash", os.environ["COMETAR_PROD_DIR"]+"/rdf_provenance/update_provenance.sh"] ,stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-#     answer = p.communicate()[0]
-#     mylog(answer)
-#     # load_into_fuseki("live")
-#     # return [get_last_provenance_date(),1]
-#     return [answer, p.returncode]
 
 def update_provenance_data(from_date,to_date):
     commits=git_commits.get_commits_list(from_date,to_date)
