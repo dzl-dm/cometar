df2ae0725f581c2680a79823153f0e22dcb3c854
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2023-07-25 11:04:06 +0200

    Remove un-used directories

--- repository/notification/get_affected_sources_by_notation.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT concept_cd, STRING_AGG(DISTINCT sourcesystem_cd, ', ')
-FROM observation_fact
-WHERE concept_cd in (<NOTATIONS>)
-GROUP BY concept_cd
-ORDER BY concept_cd
--- repository/provenance/prov_from_committers.awk
+++
@@ -1,8 +0,0 @@
-BEGIN {
-	FS=";";
-}{
-	gsub(" ","_",$1);
-	print "\n:"$1"\n\ta prov:Agent, foaf:Person;\n\tfoaf:mbox \""$2"\";\n." >> output;
-}
-END{
-}
