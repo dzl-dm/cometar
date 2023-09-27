1b4cbae66a41834b178b2c5b4e47bb4879392c37
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2023-09-27 10:56:13 +0200

    Simplify proxy image - remove certbot support

--- proxy/bootstrap-proxy.sh
+++ proxy/bootstrap-proxy.sh
@@ -25,24 +25,10 @@ fi
 IFS=$ORIG_IFS
 envsubst "\$BROWSER_FQDN \$FUSEKI_SERVER \$FUSEKI_ADMIN_ALLOW_RANGE \$GIT_SERVER \$GIT_ALLOW_RANGE \$REST_SERVER \$REST_ALLOW_RANGE \$WEB_SERVER" < /etc/nginx/conf.d/cometar-proxy.tmpl > /etc/nginx/conf.d/cometar.conf
 
-## Disable ssl if file doesn't exist - it'll cause nginx to fail!
-echo "Using FQDN: ${BROWSER_FQDN}"
-[ -f /etc/letsencrypt/live/${BROWSER_FQDN}/fullchain.pem ] ||
-  sed -i -E 's/^(\s*listen\s*443.*|\s*listen\s*\[::\]:443.*)$/#&/' /etc/nginx/conf.d/cometar.conf &&
-  sed -i -E 's/^(\s*ssl_certificate.*|\s*include \/etc\/nginx\/conf\.d\/ssl-settings\.inc.*)$/#&/' /etc/nginx/conf.d/cometar.conf
-
 ## Create auth file if doesn't already exist
 if [ ! -e /etc/nginx/auth/.htpasswd_git ]; then
   mkdir -p /etc/nginx/auth/
   touch /etc/nginx/auth/.htpasswd_git
 fi
-
-## Initial run of certbot if its required (not localhost or already setup)
-[ ${DOMAIN_LIST} = localhost ] || [ -f /etc/letsencrypt/live/${BROWSER_FQDN}/fullchain.pem ] ||
-      ( certbot certonly --standalone --agree-tos -m ${CERTBOT_EMAIL} -n -d ${DOMAIN_LIST} &&
-      echo "PATH=$PATH" > /etc/cron.d/certbot-renew  &&
-      echo "@weekly certbot renew --nginx >> /var/log/cron.log 2>&1" >>/etc/cron.d/certbot-renew &&
-      crontab /etc/cron.d/certbot-renew ) ||
-      echo "Something went wrong with the LetsEncrypt certificate setup, please check the details you've entered!"
 
 echo "---- END PROXY CONFIGURATION ----"
