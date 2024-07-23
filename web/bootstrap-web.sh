2b22f7d2cdf8ae0700482f0894f5f0ff46c5b8b1
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2024-07-23 09:14:31 +0200

    Allow injection of header/footer from external source (js based)

--- web/bootstrap-web.sh
+++ web/bootstrap-web.sh
@@ -9,6 +9,9 @@ envsubst "\$HREF_BRAND" < /config/cometar_config_template.json > /usr/share/ngin
 ## The HTML has some unclosed tags, so xmlstarlet doesn't like it
 # xmlstarlet ed -u '//title' -x "${TITLE}" /usr/share/nginx/html/cometar_browser/index.html_ORIG > /usr/share/nginx/html/cometar_browser/index.html_test
 sed "s|<title>.*</title>|<title>${TITLE}</title>|g" /usr/share/nginx/html/cometar_browser/index.html_ORIG > /usr/share/nginx/html/cometar_browser/index.html
+sed -i "s|</head>|\n${head_script}\n</head>|g" /usr/share/nginx/html/cometar_browser/index.html
+sed -i "s|<body>|<body>\n${header_placeholder}|g" /usr/share/nginx/html/cometar_browser/index.html
+sed -i "s|</body>|\n${footer_placeholder}\n<\body>|g" /usr/share/nginx/html/cometar_browser/index.html
 
 chown -R nginx:nginx /usr/share/nginx/html/cometar_browser
 echo "---- END WEB CONFIGURATION ----"
